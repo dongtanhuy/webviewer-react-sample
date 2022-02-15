f23715bf9810ff0b22505628afdcb0a787fce46c
Huy Dong <huydt@dgroup.co>
2022-02-15 15:24:07 +0700

    first build

---
+++ docs/precache-manifest.46681f162b35ebc6f989609d2155a8dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5382f552acccd6bdb449",
+    "url": "/webviewer-react-sample/static/css/main.55be0ac3.chunk.css"
+  },
+  {
+    "revision": "5382f552acccd6bdb449",
+    "url": "/webviewer-react-sample/static/js/main.5382f552.chunk.js"
+  },
+  {
+    "revision": "ed4ea6e3891188edcc4c",
+    "url": "/webviewer-react-sample/static/js/1.ed4ea6e3.chunk.js"
+  },
+  {
+    "revision": "23dd03fbedbe42b205ef",
+    "url": "/webviewer-react-sample/static/js/runtime~main.23dd03fb.js"
+  },
+  {
+    "revision": "3c48d2e73a4c1bc989647901a0510e21",
+    "url": "/webviewer-react-sample/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      // const { documentViewer, annotationManager, Annotations } = instance.Core;
       instance.UI.enableElements(['contentEditButton']);
     });
   }, []);
